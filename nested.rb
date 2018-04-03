112e8b3e57e77b1fb90c91c4e8f8dd40b6aff96a
Richard Kim <richeekim@gmail.com>
2018-04-03 20:10:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	arr = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 	return programmer_hash
 end
